5da38e135c894e4abac40eca9d57559638270455
sukhmani <sukhmani@bellevuecollege.edu>
2020-12-05 03:28:48 -0800

    component styles

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   template: '<app-server></app-server><app-server></app-server>',
-  styleUrls: ['./servers.component.css']
+ // styleUrls: ['./servers.component.css']
+styles: [`h3{color: red;
+}`]
 })
 export class ServersComponent implements OnInit {
 
