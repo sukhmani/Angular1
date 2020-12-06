b080b678e722d2564a254562ab0218322add663c
sukhmani <sukhmani@bellevuecollege.edu>
2020-12-05 20:27:44 -0800

    eventBinding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -11,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created'
-
+serverName = '';
   constructor() { 
     setTimeout(() => {
       this.allowNewServer = true;
@@ -24,8 +24,8 @@ export class ServersComponent implements OnInit {
     this.serverCreationStatus = 'Server was created'
   }
 
-  onUpdateServerName(event: any){
-    console.log(event);
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
 
   }
 
