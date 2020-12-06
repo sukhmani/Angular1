9564ff77f54a6701ac9150b044318369aa53aa97
sukhmani <sukhmani@bellevuecollege.edu>
2020-12-05 17:48:01 -0800

    additions

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning.alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-first-app/src/app/warning-alert/warning.alert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-warning-alert',
+    template: `
+    <p>This is a warning</p>`
+})
+export class WarningAlertComponent {}
