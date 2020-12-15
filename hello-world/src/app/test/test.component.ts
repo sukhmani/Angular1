a237f2c55b867614747ca2335fffc6656a99073b
sukhmani <sukhmani@bellevuecollege.edu>
2020-12-15 14:14:21 -0800

    inline template

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: '[app-test]',
-  templateUrl: './test.component.html',
+  template: '<div>Inline Template</div>',
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
