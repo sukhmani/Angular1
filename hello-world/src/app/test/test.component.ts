2dc813c4d2149d852f8f7af4c37d2960bb9e0aa3
sukhmani <sukhmani@bellevuecollege.edu>
2020-12-18 18:03:11 -0800

    ngClass to conditionally apply multiple classes

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -13,7 +13,8 @@ import { Component, OnInit } from '@angular/core';
   </div> 
   <h2 class= "text-success">style</h2>
   <h2 [class]="successClass">classBinding</h2> 
-  <h2 class="text-special" ><h2>
+  <h2 [class.text-danger]="hasError" >SelectStyleBasedOnProperty</h2>
+  <h2 [ngClass]="messageClasses">ConditionallyApplyMultipleClasses</h2>
   `,
   styles: [`
   
@@ -28,6 +29,13 @@ public name = "me";
 public myId = "testId";
 public isDisabled = false;
 public successClass = "text-success";
+public hasError = false;
+public isSpecial = true;
+public messageClasses = {
+  "text-success": !this.hasError,
+  "text-danger": this.hasError,
+  "text-special": this.isSpecial
+}
 
   constructor() { }
 
