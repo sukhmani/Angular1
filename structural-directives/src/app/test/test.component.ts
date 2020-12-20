081f69febcad4a6210d205b10dbf3cabfcc8e788
sukhmani <sukhmani@bellevuecollege.edu>
2020-12-20 15:59:09 -0800

    ngIf toggle with template

--- structural-directives/src/app/test/test.component.ts
+++ structural-directives/src/app/test/test.component.ts
@@ -2,11 +2,24 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
+  template: `
+  <h2 *ngIf = "displayName; else elseBlock">A</h2>
+
+  <ng-template #elseBlock>
+  <h2>
+  Name is hidden
+  </h2>
+  </ng-template>
+  `
+  
+  
+
+  ,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
+  displayName = false;
   constructor() { }
 
   ngOnInit(): void {
