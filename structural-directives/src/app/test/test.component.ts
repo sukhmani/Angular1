26fb63448a37aa2e5022d82b9dbb0e692ac3d806
sukhmani <sukhmani@bellevuecollege.edu>
2020-12-22 19:30:53 -0800

    pipe

--- structural-directives/src/app/test/test.component.ts
+++ structural-directives/src/app/test/test.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit, Input, Output , EventEmitter} from '@angular/core';
 
 <h2>{{"hi  " + namee}}</h2>
 <button (click)="fireEvent()"> Send Event</button>
-  
+
+<h2> {{  namee | lowercase }} </h2>
+<h2> {{ namee | uppercase }} </h2>
+<h2> {{ message | titlecase }} </h2>
+<h2> {{ namee | slice:3 }} </h2>
   
   `
   
@@ -18,6 +22,7 @@ import { Component, OnInit, Input, Output , EventEmitter} from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
+  public message = "abc";
   public color = "o";
   public colors = ["red","blue","green","yellow"];
   @Input('parentData') public namee ;
